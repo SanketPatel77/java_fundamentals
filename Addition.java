7c2a9caf245f83cb8110b3495a9672275427df3b
SanketPatel77 <sanketpatel4872@gmail.com>
2025-01-08 22:19:26 +0530

    Program for Addition

---
+++ Addition.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Addition {
+	public static void main(String [] args){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter first number");
+		int a = scan.nextInt();
+		
+		System.out.println("Enter second number");
+		int b = scan.nextInt();
+		
+		System.out.println("Sum is : " + (a+b));
+	}
+}
+		
